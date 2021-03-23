bef3de5e7a3bddf6855b351c005ab88706c1e565
khabib97 <kawser.habib.dev@gmail.com>
2021-03-23 19:22:24 +0600

    using eureka client for communication with other microservices

--- src/main/java/com/old/school/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/old/school/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-@FeignClient(name="currency-exchange", url="localhost:8000")
+//old 
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+//new
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
