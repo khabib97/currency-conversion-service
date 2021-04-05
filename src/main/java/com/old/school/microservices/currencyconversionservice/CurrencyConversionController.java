7da9b087101024cb863f006f19799a358f757654
Kawser Habib <kawser.habib.dev@gmail.com>
2021-04-05 22:55:28 +0600

    re-write to run on kubernetes

--- src/main/java/com/old/school/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/old/school/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+	
 	@Autowired
 	private CurrencyExchangeProxy currencyExchangeProxy;
 	 
@@ -21,6 +25,9 @@ public class CurrencyConversionController {
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrecnyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 		 
+		//change-kubernetes
+		logger.info("kubectl: calculateCurrencyConversion called with {} to {} with {}", from, to, quantity);
+		
 		Map<String,String> uriVariables = new HashMap();
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
@@ -36,7 +43,10 @@ public class CurrencyConversionController {
 	//using feign-latest
 	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrecnyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-		 
+		
+		//change-kubernetes 
+		logger.info("kubectl: calculateCurrencyConversion called with {} to {} with {}", from, to, quantity);
+		
 		CurrencyConversion currencyConversion = currencyExchangeProxy.retriveExchangeValue(from, to);
 		currencyConversion.setQuantity(quantity);
 		currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
--- src/main/java/com/old/school/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/old/school/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 //old 
 //@FeignClient(name="currency-exchange", url="localhost:8000")
 //new using eureka
-@FeignClient(name="currency-exchange")
+//@FeignClient(name="currency-exchange")
+//change-kubernetes
+@FeignClient(name="currency-exchange", url="${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
